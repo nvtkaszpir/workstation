2bc7b1876c737f632a6317e880469f3edd385e6f
Michal Sochon <kaszpir@gmail.com>
2019-04-26 13:44:44 +0200

    Be more verbose

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/bash -x
 
-# runs scripts on local machine
+# runs scripts on local machine under jenkins slave
 
 if [ -z "$JENKINS_URL" ]; then
   echo "Error: trying to run not on jenkins slave. Aborting."
@@ -15,6 +15,6 @@ export PATH="$HOME/.pyenv/bin:$PATH"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 
-pyenv install -f .python-version
+pyenv install -f "$WORKSPACE/.python-version"
 
 python --version
